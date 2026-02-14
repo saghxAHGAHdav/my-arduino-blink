b3fd1a41809efc2c48424ffb7fabb5b0c2dfcf0d
sephee <0012386@mail.ru>
2026-02-14 20:32:53 +0300

    SOS() has been added

--- Documents/Arduino/ArduinoBlinkProject/ArduinoBlinkProject.ino
+++ Documents/Arduino/ArduinoBlinkProject/ArduinoBlinkProject.ino
@@ -2,6 +2,12 @@ void setup() {
   pinMode(13, OUTPUT);
   pinMode(12, OUTPUT);
 }
+void blinkSOS() {
+  digitalWrite(13, HIGH); 
+  delay(10);                     
+  digitalWrite(13, LOW); 
+  delay(10);
+}
 void loop() {
   digitalWrite(12, HIGH); 
   delay(100);                     
